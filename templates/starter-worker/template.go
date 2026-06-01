ae93f33ee0694abb749709ce54ad5f6196952982
Clas Wen <clas.wen@icloud.com>
2026-06-01 21:18:01 +0800

    save

--- templates/starter-worker/template.go
+++ templates/starter-worker/template.go
@@ -2,7 +2,7 @@ package starterworker
 
 import _ "embed"
 
-// WorkerJS is the starter service-worker bundle written by the CLI.
+// WorkerJS is the starter ES-module bundle written by the CLI.
 //
 //go:embed worker.js
 var WorkerJS []byte
